ca7c6d3e6df7652a6e1426daef228925b0c95345
baigabulovs <baigabulov.s@gmail.com>
2022-07-20 21:27:16 +0600

    ÂModels Course, Student are added. Favorite 10 courses are displayed

--- educational_project/main/models.py
+++ educational_project/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название курса')
+    description = models.TextField(verbose_name='Описание курса')
+    lessons_count = models.IntegerField(verbose_name='Количество уроков')
+    lesson_duration = models.FloatField(verbose_name='Количество минут для одного урока')
+    course_logo = models.ImageField(verbose_name='Логотип')
+
+    def __str__(self):
+        return f"{self.name}: {self.description}"
+
+
+class Student(models.Model):
+    date_of_birth = models.DateField(verbose_name='Дата рождения')
+    course = models.ForeignKey(Course, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.date_of_birth}: {self.course}"
+
--- educational_project/main/views.py
+++ educational_project/main/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from . import models
 
 # Create your views here.
 def index_page(request):
     print("request is accepted")
+    # QuerySet - lazy
+    favorite_courses = models.Course.objects.all()[:10]
+
     context = {
-        "username": "Test"
+        "username": "Test",
+        "favorite_courses": favorite_courses
     }
     return render(request, "main/index.html", context)
 
