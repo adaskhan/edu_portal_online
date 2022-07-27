2c99f72a1cd2c73812caebe0820acb7308fcfd40
baigabulovs <baigabulov.s@gmail.com>
2022-07-27 21:52:11 +0600

    Lost commit

--- educational_project/main/urls.py
+++ educational_project/main/urls.py
@@ -9,5 +9,9 @@ urlpatterns = [
     path('courses', views.courses_page, name='courses_page'),
     path('results_page', views.results_page, name='results_page'),
     path('course/<int:pk>', views.course_detail_page, name='course_detail_page'),
+    path('profile', views.profile_page, name='profile_page'),
+    path('profile/delete', views.profile_delete_view, name='delete_profile'),
+    path('logout', views.logout_view, name='logout_view'),
+    path('register', views.register_page, name='register_page'),
 
 ]
--- educational_project/main/views.py
+++ educational_project/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 from . import models
 
@@ -18,6 +20,15 @@ def index_page(request):
 
 
 def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect(reverse("main:profile_page"))
+
+        return render(request, "main/login.html", {"error": "Неправильный логин/пароль"})
     return render(request, "main/login.html", {})
 
 
@@ -53,3 +64,88 @@ def course_detail_page(request, pk):
         return redirect(reverse("main:course_detail_page", args=(pk,)))
 
 
+def profile_page(request):
+    """
+    Контроллер, отвечающий за логику:
+    - отображения профиля пользователя
+    - изменения данных пользователя
+    Пользователь должен быть аутентифицирован
+    """
+
+    if request.user.is_authenticated:
+        # изменение данных пользователя при POST запросе
+        if request.method == "POST":
+            password = request.POST.get("password", None)
+            first_name = request.POST.get("first_name", None)
+            last_name = request.POST.get("last_name", None)
+
+            user = request.user
+
+            if password:
+                user.set_password(password)
+
+            if first_name:
+                user.first_name = first_name
+
+            if last_name:
+                user.last_name = last_name
+
+            user.save(update_fields=['password', 'first_name', 'last_name'])
+
+            # если было изменение пароля, значит перенаправить на страницу аутентификации
+            # так как сессия должна быть пересоздана
+            if password:
+                return redirect(reverse("main:login_page"))
+
+            return redirect(reverse("main:profile_page"))
+
+        # возвратить страницу профиля при GET запросе
+        return render(request, "main/profile.html", {})
+    return redirect(reverse("main:login_page"))
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse("main:index_page"))
+
+
+def register_page(request):
+    if request.method == "POST":
+        try:
+            # QWErty123
+            username = request.POST.get("username", None)
+            password = request.POST.get("password", None)
+            email = request.POST.get("email", None)
+            first_name = request.POST.get("first_name", None)
+            last_name = request.POST.get("last_name", None)
+            user = User.objects.create_user(
+                username=username,
+                password=password,
+                email=email,
+                first_name=first_name,
+                last_name=last_name,
+            )
+            return redirect(reverse("main:login_page"))
+        except Exception as exc:
+            print("при создании пользователя произошла ошибка", request.POST, exc)
+            error = {
+                "error_code": exc,
+                "message": "Проверьте корректность введенных данных"
+            }
+            return render(request, "main/register.html", {"error": error})
+
+    return render(request, "main/register.html", {})
+
+
+def profile_delete_view(request):
+    if request.user.is_authenticated:
+        try:
+            user = request.user
+            user.delete()
+            return redirect(reverse('main:index_page'))
+        except Exception as exc:
+            print(f"при удалении пользователя {request.user.pk} произошла ошибка", exc)
+        return redirect(reverse('main:profile_page'))
+
+    # Если пользователь не аутентифицирован, перенаправить на страницу логина
+    return redirect(reverse('main:login_page'))
